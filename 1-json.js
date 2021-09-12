f77e56ef14aa63f2be5a495bfdd2064bc1d34b54
koval2001 <48998783+koval2001@users.noreply.github.com>
2021-09-12 18:17:55 +0300

    added: new js and json files

---
+++ 1-json.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+
+/*const book = {
+    title: 'Ego is the Enemy',
+    author: 'Ryan Holiday'
+}
+
+const bookJSON = JSON.stringify(book)
+console.log(bookJSON)
+
+fs.writeFileSync('1-json.json', bookJSON)
+*/
+
+const dataBuffer = fs.readFileSync('1-json.json')
+const dataJSON = dataBuffer.toString()
+const data = JSON.parse(dataJSON)
+
+data.name = "Diana"
+data.age = "20"
+
+const myData = JSON.stringify(data)
+fs.writeFileSync('1-json.json', myData)
+
+console.log(data)
